63e65f695e4fc6646d107279f3aa88e0a5daa83e
Honeisideleon <honeissydeleon@gmail.com>
2026-03-18 23:15:20 -0400

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿namespace MauiApp7B;
+
+public partial class MainPage : ContentPage
+{
+    // Variable para saber el estado
+    bool encendido = false;
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnBotonClicked_IfElse(object sender, EventArgs e)
+    {
+        // Cambiamos el valor
+        encendido = !encendido;
+
+        // Estructura IF/ELSE para cambiar el dibujo
+        if (encendido)
+        {
+            // Foco encendido
+            bombilloEmoji.Text = "💡";
+        }
+        else
+        {
+            // Círculo negro (apagado)
+            bombilloEmoji.Text = "⚫";
+        }
+    }
+}
